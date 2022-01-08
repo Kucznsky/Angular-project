51cc92b463bd702ceb6717fc499a06b0646b2bc4
Daniel Pietrzeniuk <ProgramistycznySwir@gmail.com>
2022-01-08 21:30:35 +0100

    Scaffolding

--- AngularCinema/src/app/app-routing.module.ts
+++ AngularCinema/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomePageComponent },
+  /* Wszystkie ścieżki trzeba dawać przed tą ścieżką. */
+  { path: '**', redirectTo: 'home' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
